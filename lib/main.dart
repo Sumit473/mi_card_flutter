4a31433591bb96db4fc5662bf8401fe0544456d7
Sumit Garg <gargs473@gmail.com>
2020-09-05 18:15:35 +0530

    Learn about Column and Row Widgets(widgets with children)

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,38 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.teal,
-          body: Container(
-            color: Colors.white,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(20),
-            child: Text('Hello World!'),
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.orange,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Text('Container1'),
+              ),
+
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Text('Container2'),
+              ),
+
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.green,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Text('Container3'),
+              ),
+            ]
           ),
         ),
       ),
