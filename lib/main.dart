f130ffe72c94c53e8692baabd7cd28887d93a3f6
Sumit Garg <gargs473@gmail.com>
2020-09-05 17:43:57 +0530

    Learn about Container Widget(a single child widget) and some more basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Container(
+            color: Colors.white,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            child: Text('Hello World!'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
